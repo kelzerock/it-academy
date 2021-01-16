06e3c1b4572dca17a28d3a79c1d6636ad2a1eb16
kelzerock <76631388+kelzerock@users.noreply.github.com>
2021-01-16 22:33:49 +0300

    Дороботка
    Дороботал

--- anketa_js.js
+++ anketa_js.js
@@ -4,25 +4,25 @@ var lastName = prompt('введите вашу Фамилию');
     var lastName = prompt('введите вашу Фамилию корректно');
   } 
 var firstName = prompt('введите ваше Имя');
-    while (Boolean(firstName) == false || isNaN(Number(firstName))==false) {
+    while (Boolean(firstName) == false || isNaN(Number(firstName)) == false) {
     var firstName = prompt('введите ваше Имя корректно');
     } 
 var otchestvo = prompt('введите ваше Отчество');
-    while (Boolean(otchestvo) == false || isNaN(Number(otchestvo))==false) {
+    while (Boolean(otchestvo) == false || isNaN(Number(otchestvo)) == false) {
       var otchestvo = prompt('введите ваше Отчетсво корректно');
       } 
 var ages = prompt('Укажите ваш возвраст, в годах');
 ages = Number(ages)
-      while (isNaN(ages) == true)
+      while (Boolean(ages) == false || ages < 0)
       {var ages = prompt('Укажите ваш возвраст корректно, в годах');
       ages = Number(ages)
       }
-var gender = confirm('Если вы мужского пола нажмите "OK"');
+var gender = confirm('Если вы мужского пола нажмите "OK", если женского - то нажмите "Отмена" или клавишу "esc"');
 var genderName = gender? 'мужской': 'женский';
 var pensia
-if (genderName == 'мужской')
-{ages >65? pensia = 'да' :  pensia = 'нет'}
-else {ages >60?  pensia = 'да' :  pensia = 'нет'}
+    if (gender)
+    {ages >65? pensia = 'да' :  pensia = 'нет'}
+    else {ages >60?  pensia = 'да' :  pensia = 'нет'}
 alert(
 'ваше ФИО: '+ lastName + ' ' + firstName + ' ' + otchestvo + 
 '\n ваш возраст в годах: ' + ages +
