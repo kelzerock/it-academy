dc053511a5d66b8ff6a0e80ca434c3458c656533
kelzerock <76631388+kelzerock@users.noreply.github.com>
2021-01-25 23:29:21 +0300

    Update n05.js

--- n05.js
+++ n05.js
@@ -1,17 +1,17 @@
 "use strict"
 
-
+//согласно условиям задачи перед нами либо числа, либо массивы из чисел поэтому функции по преобразованию данных в числа не использовались.
 
 function treeSum(massiv){
-  let summ=0 //вводим и идентифицируем переменную с начальным значением "0"
+  let totalSumm=0 //вводим и идентифицируем переменную с начальным значением "0"
     for (var i = 0; i < massiv.length; i++) { //цикл по каждому елементу массива
       if (Array.isArray(massiv[i])) { //определяем перед нами массив или нет
-      summ+= treeSum(massiv[i]); //е
-      console.log(summ);
-      } else { summ += massiv[i];
-        console.log(summ); }
+      totalSumm+= treeSum(massiv[i]); //если перед нами массив запускаем в нее дальше нашу фунцию пока она не достигнет чисел
+      console.log(totalSumm);
+      } else { totalSumm += massiv[i]; //если перед нами не массив суммируем это число в переменную totalSumm
+      console.log(totalSumm); }
     }
-return summ
+return totalSumm //возвращаем итог
 }
 let nn = [ 5, 7, 
   [ 4, [2], 8, [1,3], 2 ], 
