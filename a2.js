2fa1636c9f2f124780ef1b5c663c82602e356298
kelzerock <76631388+kelzerock@users.noreply.github.com>
2021-01-26 00:19:09 +0300

    Оптимазация задания a2

--- a2.js
+++ a2.js
@@ -7,14 +7,30 @@ function question(){
 function clearString(x){
   let leftSpace = 0;
   let rightSpace = 0;
-  for(i=0;x[i] == ' ' && i < x.length; ++i) {
+  let i = 0;
+  let y = 1;
+    if(x[i] != ' ' && x[x.length-y] != ' ') { //проверка, если пробелов нет в начале и конце строки - возвращаем 'x'
+      console.log('Пробелов нет');
+      return x;
+    }
+
+    while (x[i] == ' ' && i < x.length) {
+    ++i;
     leftSpace = i+1;
     console.log(i)
-  }
-  for(y=1;(x[x.length-y] == ' ') && (i < x.length); ++y) {
+    }
+    if(i==x.length-1) {
+      console.log('сработал только 1 цикл - строка из пробелов'); // если строка состоит только из пробелов 2-ой цикл даже не запускаем.
+      return x='';
+    } else {
+      console.log('продолжаем работу')
+    }
+
+    while((x[x.length-y] == ' ') && (i < x.length)) {
+    ++y;
     rightSpace = y;
     console.log(y)
-  }
+    }
   return x = x.slice(leftSpace,x.length-rightSpace)
 } 
 
