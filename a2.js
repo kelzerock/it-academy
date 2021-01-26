008cbba16e653408435b21e9d72164c3be2ba53a
kelzerock <76631388+kelzerock@users.noreply.github.com>
2021-01-26 11:40:35 +0300

    Update a2.js

--- a2.js
+++ a2.js
@@ -8,28 +8,26 @@ function clearString(x){
   let leftSpace = 0;
   let rightSpace = 0;
   let i = 0;
-  let y = 1;
-    if(x[i] != ' ' && x[x.length-y] != ' ') { //проверка, если пробелов нет в начале и конце строки - возвращаем 'x'
+  let y = 0;
+    if(x[i] != ' ' && x[x.length-1] != ' ') { //проверка, если пробелов нет в начале и конце строки - возвращаем 'x'
       console.log('Пробелов нет');
       return x;
     }
 
     while (x[i] == ' ' && i < x.length) {
     ++i;
-    leftSpace = i+1;
-    console.log(i)
+    leftSpace = i;
     }
-    if(i==x.length-1) {
+    if(i==x.length) {
       console.log('сработал только 1 цикл - строка из пробелов'); // если строка состоит только из пробелов 2-ой цикл даже не запускаем.
       return x='';
     } else {
       console.log('продолжаем работу')
     }
 
-    while((x[x.length-y] == ' ') && (i < x.length)) {
+    while((x[x.length-y-1] == ' ') && (y < x.length)) {
     ++y;
     rightSpace = y;
-    console.log(y)
     }
   return x = x.slice(leftSpace,x.length-rightSpace)
 } 
