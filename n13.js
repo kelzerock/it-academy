f5b93771db3877caab219614325d585909d194d6
kelzerock <76631388+kelzerock@users.noreply.github.com>
2021-02-10 22:12:40 +0300

    Update n13.js

--- n13.js
+++ n13.js
@@ -19,66 +19,10 @@ function HashStorageFunc(key, value){
   }
 
   self.getKeys = function(){
-      for (var keysMassive in self) {
-      console.log(keysMassive);
-      }
+    return Object.keys(self);
   }
 }
-var drinkStorage = new HashStorageFunc('information', 'alco',);
+var drinkStorage = new HashStorageFunc();
 drinkStorage.addValue('vodka', 'water and spirt');
 drinkStorage.addValue('beer', 'water and hmel');
 drinkStorage.getKeys()
-
-  self.listValue= listValue= {};
-  self.addValue = function(key,value){
-    listValue.key = value
-  }
-  self.getValue = function(key){
-    if(key in listValue) {
-      console.log(listValue.key)
-    } else {undefined}
-
-  }
-  self.deleteValue = function(key){ 
-   if (key in listValue){
-     delete listValue.key;
-     console.log(true)
-   } else {console.log(false)}
-  }
-  self.getKeys = function(){
-       let keysMassive = []
-       for (key in listValue)
-       return console.log(keysMassive.push(key))
-  }
-
-}
-
-var drinkStorage = new HashStorageFunc('information', 'alco', 'recepter')
-
-// function HashStorageFunc(key, value){
-//   var self = this;
-//   self.listValue = {};
-//   self.addValue = function(key,value){
-//     listValue.key = value
-//   }
-//   self.getValue = function(key){
-//     if(key in listValue) {
-//       console.log(listValue.key)
-//     } else {undefined}
-
-//   }
-//   self.deleteValue = function(key){ 
-//    if (key in listValue){
-//      delete listValue.key;
-//      console.log(true)
-//    } else {console.log(false)}
-//   }
-//   self.getKeys = function(key){
-//        let keysMassive = []
-//        for (key in listValue)
-//        return console.log(keysMassive.push(key))
-//   }
-
-// }
-
-// var drinkStorage = new HashStorageFunc('information', 'alco', 'recepter')
